e4071766cdb46209820d350fdf76612845fc9557
DansZbar2 <40437965+DansZbar2@users.noreply.github.com>
2019-08-01 21:06:26 +0300

    Update armorStuff.js

--- dev/api/armorStuff.js
+++ dev/api/armorStuff.js
@@ -3,32 +3,32 @@ AE:[],
 IEt:[],    
 addEffectToArmor:function(params){
 Callback.addCallback("tick", function(){    
-if(Entity.getArmorSlot(this.params.entity, this.params.slot).id == this.params.id){
-Entity.addEffect(this.params.entity, this.params.effectID, this.params.effectTime, this.params.powerLVL, false, false);   
+if(Entity.getArmorSlot(params.entity, params.slot).id == params.id){
+Entity.addEffect(params.entity, params.effectID, params.effectTime, params.powerLVL, false, false);   
   }
 });
 },
 addArmorToEntity:function(params){
-if(!this.params.entity)
+if(!params.entity)
 return Logger.LogError("{addArmorToEntity} params.entity должен быть строкой или числом", "EntitysSS");
-if(!this.params.slot)
+if(!params.slot)
 return Logger.LogError("{addArmorToEntity} params.slot должен быть числом", "EntitysSS");    
-if(this.params.id)
+if(!params.id)
 return Logger.LogError("{addArmorToEntity} params.id должен быть строкой или числом", "EntitysSS");
-if(this.params.data)
+if(!params.data)
 return Logger.LogError("{addArmorToEntity} params.data должен быть числом", "EntitysSS");        
 AE.push(params);
 },
 addItemToHand:function(params){
-if(!this.params.entity)
+if(!params.entity)
 return Logger.LogError("{addItemToHand} params.entity должен быть строкой или числом", "EntitysSS");
-if(!this.params.slot)  
+if(!params.slot)  
 return Logger.LogError("{addItemToHand} params.slot должен быть числом", "EntitysSS");    
-if(this.params.id)    
+if(!params.id)    
 return Logger.LogError("{addItemToHand} params.id должен быть строкой или числом", "EntitysSS");
-if(this.params.count)
+if(!params.count)
 return Logger.LogError("{addItemToHand} params.data должен быть числом", "EntitysSS");  
-if(this.params.data)
+if(!params.data)
 return Logger.LogError("{addItemToHand} params.data должен быть числом", "EntitysSS");  
 AI.push(params);
 },
