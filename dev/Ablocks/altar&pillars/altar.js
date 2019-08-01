72173747cb3a24fa7469d48fb0752d9da7f7debc
DansZbar2 <40437965+DansZbar2@users.noreply.github.com>
2019-08-01 22:04:40 +0300

    some new feature
    model for fox,new block

---
+++ dev/Ablocks/altar&pillars/altar.js
@@ -0,0 +1,4 @@
+IDRegistry.genBlockID("AltarAbyssal");
+Block.createBlockWithRotation("AltarAbysal", [
+{name: "Abyssal Altar", texture: [["DLGbottom", 0], ["DLGtop", 0], ["DLGsides", 0], ["Altar", 0], ["Altar", 0], ["Altar", 0]], inCreative: true}], "opaque");
+ToolAPI.registerBlockMaterial(BlockID.dirtDark, "dirt", 0, true);
--- dev/Ablocks/terrain.js
+++ dev/Ablocks/terrain.js
@@ -1,4 +1,9 @@
 IDRegistry.genBlockID("stoneDark"); 
 Block.createBlock("stoneDark", [
-    {name: "Dark Stone", texture: [["DS", 0]],inCreative: true}], "opaque");
+{name: "Dark Stone", texture: [["DS", 0]],inCreative: true}], "opaque");
 ToolAPI.registerBlockMaterial(BlockID.stoneDark, "stone", 1, true);
+
+IDRegistry.genBlockID("dirtDark");
+Block.createBlockWithRotation("dirtDark", [
+{name: "DarkLends Dirt", texture: [["DLGbottom", 0], ["DLGtop", 0], ["DLGsides", 0], ["DLGsides", 0], ["DLGsides", 0], ["DLGsides", 0]], inCreative: true}], "opaque");
+ToolAPI.registerBlockMaterial(BlockID.dirtDark, "dirt", 0, true);
